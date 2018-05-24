55b0478d53de6b646bf7dc76f6406001f5c6235a
alexandrefett@gmail.com <tuneca2011>
2018-05-24 18:07:34 -0300

    firestore

--- lib/instamanager.dart
+++ lib/instamanager.dart
@@ -7,15 +7,33 @@ import 'package:insta_app/user.dart';
 class InstaManager {
   static final InstaManager _instamanager = new InstaManager._internal();
 
-  static const SERVER = 'http://192.168.0.18/api/v1';
+  static const SERVER = 'http://192.168.0.18/api/v1/{{method}}';
+  static const GET_PROFILE = '/profile/{{token}}';
+  static const POST_PROFILE = '/register';
 
   factory InstaManager() {
     return _instamanager;
   }
   InstaManager._internal();
 
-  Future<User> _getProfile(String username) async {
-    http.Response response = await http.get(
+  String _getMethod(String method){
+    return SERVER.replaceAll('{{method}}', method);
+  }
+
+  Future<User> _getProfile(String token) async {
+    String url = _getMethod(GET_PROFILE);
+    url.replaceAll('{{token}}', token);
+    print(url);
+    http.Response response = await http.get(url,
+        headers: {"Accept": "application/json"});
+    print('URL= ' + response.request.url.toString());
+    print(response.body);
+    Map data = json.decode(response.body);
+    return new User.fromJson(data);
+  }
+
+  Future<User> _postProfile(User user) async {
+    http.Response response = await http.post(url)get(
         "http://10.125.121.64:4567/api/v1/user/$username",
         headers: {"Accept": "application/json"});
     print('URL= ' + response.request.url.toString());
