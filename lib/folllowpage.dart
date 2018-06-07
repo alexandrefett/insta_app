8affdee882ed37249ca1ccd0953f6ef7e92a68d2
Alexandre <alexandrefett@gmail.com>
2018-06-07 06:59:58 -0300

    profile

--- lib/folllowpage.dart
+++ lib/folllowpage.dart
@@ -13,32 +13,27 @@ class FollowsPage extends StatefulWidget {
 class _FollowsPage extends State<FollowsPage>
     with AutomaticKeepAliveClientMixin<FollowsPage> {
   Session session = new Session();
+  bool _isLoading = false;
   var data = List<Account>();
   var pageinfo = new PageInfo(hasNextPage: true, endCursor: "");
   var nextPage;
   //Follows page;
 
   Future<List<Account>> _getFollows(int id, PageInfo pageInfo) async {
-    List<Account> datas = new List<Account>();
     String url = Endpoint.GET_FOLLOWS +
         '?id=$id&hasNext=${pageInfo.hasNextPage}&cursor=${pageInfo.endCursor}';
     Map map = await session.get(url);
+    print(url);
     print(map);
-
-    datas = data;
     List acc = map['data']['user']['edge_follow']['edges'] as List;
     acc.forEach((element) {
-      print(element);
       Map node = element['node'];
-      print(node);
       Account a = Account.fromInsta(node);
-      print('-----1');
-      datas.add(a);
-      print('-----2');
+      data.add(a);
     });
     nextPage =
         PageInfo.fromJson(map['data']['user']['edge_follow']['page_info']);
-    return datas;
+    return data;
   }
 
   @override
@@ -46,38 +41,53 @@ class _FollowsPage extends State<FollowsPage>
     return new FutureBuilder<List<Account>>(
         future: _getFollows(Singleton.instance.account.id, pageinfo),
         builder: (BuildContext context, AsyncSnapshot<List<Account>> snapshot) {
-          if (snapshot.hasData) {
+          if (snapshot.connectionState==ConnectionState.done) {
+            print(snapshot.connectionState);
             return createListView(context, snapshot);
-           /* ListView.builder(
-                itemCount: (snapshot.data == null) ? 0 : snapshot.data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  print(index);
-                  print(pageinfo.hasNextPage);
-                  if (index == snapshot.data.length && pageinfo.hasNextPage) {
-                    setState(() {
-                      pageinfo = nextPage;
-                      data = snapshot.data;
-                    });
-                  }
-                  Account account = snapshot.data[index];
-                  return new ListTile(title: new Text(account.username));
-                });
-                */
-          } else {
-            return _buildProgress;
           }
+          else
+            return _buildProgress;
         });
   }
 
-  Widget _buildProgress = new Center(child: new CircularProgressIndicator());
+  Future<void> _updatePage() async{
+    new Future.delayed(new Duration(milliseconds: 300), ()
+    {
+      setState(() {
+        pageinfo = nextPage;
+      });
+    });
+  }
+
+  Widget _buildProgress = new Center(
+      child: new SizedBox(
+          width: 18.0,
+          height: 18.0,
+          child: new CircularProgressIndicator()));
 
   Widget createListView(BuildContext context, AsyncSnapshot snapshot) {
     List<Account> values = snapshot.data;
     return new ListView.builder(
-        itemCount: values.length,
+        itemCount: pageinfo.hasNextPage ? values.length + 1: values.length,
         itemBuilder: (BuildContext context, int index) {
+          print(index);
+          if(index==values.length && pageinfo.hasNextPage){
+            _updatePage();
+            return _buildProgress;
+          }
           return new ListTile(
-            title: new Text(values[index].username),
+            leading: new CircleAvatar(
+              backgroundImage: new NetworkImage(values[index].profilePicUrl),
+            ),
+            title: new Text(
+              values[index].username,
+              style: new TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: new Text(values[index].fullName),
+            trailing: new FlatButton(
+                onPressed: () {
+                },
+                child: new Text('Follow')),
           );
         });
   }
